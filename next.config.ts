346bf4e8bcfa11dc3d28613186c45c6076b66e3c
shameem <shameemkk4@gmail.com>
2026-03-02 10:28:20 +0530

    feat: add Dockerfile and .dockerignore for containerization setup

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: false,
   experimental: {
     serverActions: {
-      allowedOrigins: ["*.inc1.devtunnels.ms", "localhost:3000"],
+      allowedOrigins: ["*"],
     },
   },
 };
